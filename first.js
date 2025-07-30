331fc80a486eaf747d2b365dd74996dcfc017f09
maximus116 <maximus-116@mail.ru>
2025-07-31 00:42:38 +0300

    Add files via upload

--- first.js
+++ first.js
@@ -1,18 +1,14 @@
 function open_btn(el) {
-  var k = document.getElementById('menu_ul');
-  k.style.display = "block";
-  var b = document.getElementsByClassName('burger');
-  b.style.display = 'block';
-
+  /*  var k = document.getElementById('menu_ul');
+    k.style.display = "block";*/
+  /*var b = document.getElementsByClassName('burger');
+  b.style.display = 'block';*/
+  var k = document.getElementById("menu_ul");
+  k.classList.remove("mystyle");
 }
 function close_btn(el) {
-  var k = document.getElementById('menu_ul');
-  k.style.display = "none";
+  var k = document.getElementById("menu_ul");
+  k.classList.add("mystyle");
+  /*var k = document.getElementById('menu_ul');
+  k.style.display = "none";*/
 }
-window.addEventListener('resize', function () {
-  let width_local = window.innerWidth;
-  if (width_local > 980){
-                          let k = document.getElementById('menu_ul');
-                          k.style.display = "block";
-                         } 
-});
