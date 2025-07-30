db74b7655c7987af4cfcc67531adc50e9d39a00b
maximus116 <maximus-116@mail.ru>
2025-07-30 05:32:31 +0300

    Add files via upload

---
+++ first.js
@@ -0,0 +1,11 @@
+    function open_btn(el) {
+      var k = document.getElementById('menu_ul');
+      k.style.display = "block";
+      var b = document.getElementsByClassName('burger');
+      b.style.display = 'block';
+
+    }
+    function close_btn(el) {
+      var k = document.getElementById('menu_ul');
+      k.style.display = "none";
+    }
