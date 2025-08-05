09f1b3a8350580a4d7d77d770faa8e345487ba71
maximus116 <maximus-116@mail.ru>
2025-08-05 23:49:59 +0300

    Add files via upload

--- first.js
+++ first.js
@@ -1,14 +1,33 @@
-function open_btn(el) {
-  /*  var k = document.getElementById('menu_ul');
-    k.style.display = "block";*/
-  /*var b = document.getElementsByClassName('burger');
-  b.style.display = 'block';*/
-  var k = document.getElementById("menu_ul");
-  k.classList.remove("mystyle");
-}
-function close_btn(el) {
+function showMenu(el) {
   var k = document.getElementById("menu_ul");
   k.classList.add("mystyle");
-  /*var k = document.getElementById('menu_ul');
-  k.style.display = "none";*/
 }
+function CloseMenu(el) {
+  var l = document.getElementById("menu_ul");
+  l.classList.remove("mystyle");
+
+}
+window.addEventListener('scroll', function (event) {
+  // Изменяем стили элемента при прокрутке страницы
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > 60) {
+    var l = document.getElementById('navbar_bottom');
+    l.classList.add("navbar_bottom_scrol");
+    l.classList.add("wrap_navbar_bottom");
+  }
+    else {
+    var l = document.getElementById('navbar_bottom');
+    l.classList.remove("navbar_bottom_scrol");
+    l.classList.remove("navbar_bottom_scrol1");
+  }
+  if (scrollTop > 100) {
+    var l = document.getElementById('navbar_bottom');
+    l.classList.add("navbar_bottom_scrol1");
+    l.classList.add("wrap_navbar_bottom_scrol");
+  }
+  else {
+    var l = document.getElementById('navbar_bottom');
+     l.classList.remove("navbar_bottom_scrol1");
+     l.classList.remove("wrap_navbar_bottom_scrol");
+  }
+});
